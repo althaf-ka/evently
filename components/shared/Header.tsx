b70fc06499fa0850b7ea974899be3718ecfa322d
Althaf <althafrahim101@gmail.com>
2024-02-03 21:28:46 +0530

    Setup the Project and Added Header

---
+++ components/shared/Header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently Logo"
+          />
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3">
+          {/* Clerk Login Button */}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
