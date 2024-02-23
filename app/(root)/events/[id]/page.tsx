a87284471ec48530ccc98e0db594dfe6d0bb0a7e
Althaf <althafrahim101@gmail.com>
2024-02-23 22:22:08 +0530

    Integrated Stripe for ticket bookings, adjusted currency to INR.

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
 import {
   getEventById,
@@ -37,7 +38,7 @@ const EventDetails = async ({
               <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
                 <div className="flex gap-3">
                   <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
-                    {event.isFree ? "FREE" : `$${event.price}`}
+                    {event.isFree ? "FREE" : `₹ ${event.price}`}
                   </p>
 
                   <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
@@ -54,7 +55,7 @@ const EventDetails = async ({
               </div>
             </div>
 
-            {/* CHECKOUT BTN */}
+            <CheckoutButton event={event} />
 
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
