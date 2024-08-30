967a5adf249b1b911eb1777250e5d5878ae53381
Althaf K.A <116885358+althaf-ka@users.noreply.github.com>
2024-08-30 21:38:28 +0530

    Added update events feature.

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,9 +1,18 @@
 import EventForm from "@/components/shared/EventForm";
+import { getEventById } from "@/lib/actions/events.actions";
 import { auth } from "@clerk/nextjs";
 
-const UpdateEvent = () => {
+type UpdateEventProps = {
+  params: {
+    id: string;
+  };
+};
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
+
+  const event = await getEventById(id);
 
   return (
     <>
@@ -13,7 +22,12 @@ const UpdateEvent = () => {
         </h3>
       </section>
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Update" />
+        <EventForm
+          userId={userId}
+          type="Update"
+          event={event}
+          eventId={event._id}
+        />
       </div>
     </>
   );
